89b2d7d9edb3b4d7d87d2f2fb8e2ab517e2f6057
Amit Joe <90870511+Amit-Joe-coder@users.noreply.github.com>
2022-09-20 16:35:28 +0530

    Update c45.c

--- c45.c
+++ c45.c
@@ -1,9 +1,3 @@
-/*Problem
-Janmansh and Jay are playing a game. They start with a number XX and they play a total of YY moves. Janmansh plays the first move of the game, after which both the players make moves alternatingly.
-
-In one move, a player can increment or decrement XX by 11.
-
-If the final number after performing YY moves is even, then Janmansh wins otherwise, Jay wins. Determine the winner of the game if both the players play optimally.*/
 #include <stdio.h>
 // void chances(int, int);
 int main()
